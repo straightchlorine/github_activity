449ea7cc1aa93d8ec0a91bccafc71ba616ed31be
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-11-22 15:19:41 +0100

    activity: possibility of choosing a user to check
    Basically username is no longer hardcoded

--- lib/main.dart
+++ lib/main.dart
@@ -22,44 +22,66 @@ class GitHubActivityScreen extends StatefulWidget {
 class _GitHubActivityScreenState extends State<GitHubActivityScreen> {
   final GitHubService _gitHubService = GitHubService();
   late List<GitHubActivity> _activities;
-  final String _username = 'straightchlorine';
+  TextEditingController _usernameController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _loadGitHubActivity();
+    _activities = [];
   }
 
   void _loadGitHubActivity() async {
-    try {
-      final activities = await _gitHubService.getGitHubActivity(_username);
-      setState(() {
-        _activities = activities;
-      });
-    } catch (e) {
-      print('Error: $e');
+    final username = _usernameController.text;
+    if (username.isNotEmpty) {
+      try {
+        final activities = await _gitHubService.getGitHubActivity(username);
+        setState(() {
+          _activities = activities;
+        });
+      } catch (e) {
+        print('Error: $e');
+      }
+    } else {
+      print('Please enter a GitHub username');
     }
   }
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('GitHub Activity'),
       ),
-      body: _activities == null
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: _activities.length,
-              itemBuilder: (context, index) {
-                final activity = _activities[index];
-                return ListTile(
-                  title: Text(activity.type),
-                  subtitle: Text('${activity.actorLogin} - ${activity.repoName}'),
-                  trailing: Text(activity.createdAt.toString()),
-                );
-              },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'GitHub Username'),
             ),
+          ),
+          ElevatedButton(
+            onPressed: _loadGitHubActivity,
+            child: Text('Fetch GitHub Activity'),
+          ),
+          Expanded(
+            child: _activities.isEmpty
+                ? Center(child: Text('No GitHub activity to display'))
+                : ListView.builder(
+                    itemCount: _activities.length,
+                    itemBuilder: (context, index) {
+                      final activity = _activities[index];
+                      return ListTile(
+                        title: Text(activity.type),
+                        subtitle: Text('${activity.actorLogin} - ${activity.repoName}'),
+                        trailing: Text(activity.createdAt.toString()),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
     );
   }
 }
