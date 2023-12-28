5bb2e3b549aa66a06aa9394d4401d24d88f3519a
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-12-28 12:14:24 +0100

    sharedprefs: added shared prefs for darkmode

--- lib/state_provider.dart
+++ lib/state_provider.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
 import 'package:github_activity/user_object.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppState extends ChangeNotifier {
+
   bool _isDarkMode = false;
   bool get isDarkMode => _isDarkMode;
 
@@ -12,8 +16,9 @@ class AppState extends ChangeNotifier {
   User? get current_user => _current_user;
   void set current_user(User? user) => _current_user = user;
 
-  void toggleTheme() {
+  void toggleTheme(prefs) {
     _isDarkMode = !_isDarkMode;
+    prefs.setBool('isDarkMode', _isDarkMode);
     notifyListeners();
   }
 
@@ -22,11 +27,16 @@ class AppState extends ChangeNotifier {
       if (current_user?.login == user?.login)
         _fetched.remove(current_user);
         _fetched.add(user);
+    
     notifyListeners();
   }
 
   void loadStoredActivity(User? user) async {
     _current_user = user;
     notifyListeners();
+  }
+
+  void set isDarkMode(bool pref) {
+    _isDarkMode = pref;
   }
 }
